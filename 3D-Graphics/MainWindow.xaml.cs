59144a66a38114606131681c1cacd5bb17f49a1f
Szymon Zyguła <szymon@zygula.eu>
2021-12-28 18:05:32 +0100

    Add fast textures

--- 3D-Graphics/MainWindow.xaml.cs
+++ 3D-Graphics/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +15,39 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace _3D_Graphics {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
+        Texture DrawingPlane;
+        double a;
+        int frames;
+        Stopwatch sw;
+
         public MainWindow() {
             InitializeComponent();
+
+            CompositionTarget.Rendering += RenderFrame;
+
+            DrawingPlane = new Texture((int)ImageCanvas.Width, (int)ImageCanvas.Height);
+            DrawingPlane.Clean(new Vec3(0.5, 0.25, 0.75));
+            ImageCanvas.Source = DrawingPlane.CreateBitmapSource();
+            a = 0.0;
+            frames = 0;
+
+            sw = new Stopwatch();
+
+            sw.Start();
+        }
+
+        private void RenderFrame(object sender, EventArgs e) {
+            a += 0.01;
+            DrawingPlane.Clean(new Vec3(0.5, 0.75, 0.25));
+            DrawingPlane.Pixels[(int)((DrawingPlane.Width - 20) * (Math.Cos(a) / 2 + 0.5)), (int)((DrawingPlane.Height - 20) * (Math.Sin(a) / 2 + 0.5))] = new Vec3(0.0, 0.0, 0.0);
+            ImageCanvas.Source = DrawingPlane.CreateBitmapSource();
+            frames += 1;
+            if(frames % 100 == 0) {
+                sw.Stop();
+                MessageBox.Show($"Average FPS: {(double)frames / (double)sw.ElapsedMilliseconds * 1000}");
+                sw.Start();
+            }
         }
     }
 }
--- 3D-Graphics/Texture.cs
+++ 3D-Graphics/Texture.cs
@@ -1,22 +1,33 @@
 ﻿using System;
 using System.Drawing;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Threading.Tasks;
 
 namespace _3D_Graphics {
-
     // This class has to be used instead of System.Drawing.Bitmap,
     // because System.Drawing.Bitmap.GetPixel too slow to be used in real time rendering
     public class Texture {
         public Vec3[,] Pixels;
-        public int Width { get { return Coefficients.GetLength(0); } }
-        public int Height { get { return Coefficients.GetLength(1); } }
+        private UInt32[] RawPixels;
+
+        public int Width { get { return Pixels.GetLength(0); } }
+        public int Height { get { return Pixels.GetLength(1); } }
+
+        public Texture(int width, int height) {
+            Pixels = new Vec3[width, height];
+            RawPixels = new UInt32[Width * Height];
+        }
+
         public Texture(Bitmap bitmap) {
             GraphicsUnit unit = GraphicsUnit.Pixel;
             RectangleF bounds = bitmap.GetBounds(ref unit);
 
-            Pixels = new Vec3[bounds.Width, bounds.Height];
+            Pixels = new Vec3[(int)bounds.Width, (int)bounds.Height];
+            RawPixels = new UInt32[Width * Height];
 
-            for(int x = 0; x < bounds.Width; ++x) {
-                for(int y = 0; y < bounds.Height; ++y) {
+            for(int x = 0; x < Width; ++x) {
+                for(int y = 0; y < Height; ++y) {
                     int color = bitmap.GetPixel(x, y).ToArgb();
                     UInt32 convertedColor;
                     unsafe {
@@ -26,6 +37,46 @@ namespace _3D_Graphics {
                     Pixels[x, y] = new Vec3(convertedColor);
                 }
             }
+        }
+
+        private Texture(Texture texture) {
+            Pixels = new Vec3[texture.Width, texture.Height];
+            RawPixels = new UInt32[Width * Height];
+            for (int x = 0; x < Width; ++x) {
+                for (int y = 0; y < Height; ++y) {
+                    Pixels[x, y] = texture.Pixels[x, y];
+                }
+            }
+        }
+
+        public Texture Clone() {
+            Texture clone = new Texture(this);
+            return clone;
+        }
+        
+        public void Clean(Vec3 color) {
+            Parallel.For(0, Width, (x) => {
+                for (int y = 0; y < Height; ++y) {
+                    Pixels[x, y] = color;
+                }
+            });
+        }
+
+        public BitmapSource CreateBitmapSource() {
+            Parallel.For(0, Width, (x) => {
+                for(int y = 0; y < Height; ++y) {
+                    RawPixels[x + y * Width] = Pixels[x, y].ToColor();
+                }
+            });
+
+            return BitmapSource.Create(
+                Width, Height,
+                96, 96,
+                PixelFormats.Bgra32,
+                null,
+                RawPixels,
+                Width * 4
+            );
         }
     }
 }
